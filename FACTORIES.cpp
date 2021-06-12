0d1cd57e9a42dbcfe2a40ff25235ebcdec9c830d
Pupil <78217280+hocvn@users.noreply.github.com>
2021-06-12 21:03:50 +0700

    Update FACTORIES.cpp

--- FACTORIES.cpp
+++ FACTORIES.cpp
@@ -72,8 +72,6 @@ void solve(){
 }
 int main(){
 
-	freopen("FACTORIES.inp","r",stdin);
-	freopen("FACTORIES.out","w",stdout);
 	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 	cin >> t;
 	while (t--) solve();
