e8e11121f4c3d23750e30e27bee08a1f33e87d6d
hocvn <78217280+hocvn@users.noreply.github.com>
2022-03-01 11:10:17 +0700

    Update HeavyLight.cpp

--- HeavyLight.cpp
+++ HeavyLight.cpp
@@ -1,106 +1,103 @@
-//https://oj.vnoi.info/problem/qtree3
+// https://oj.vnoi.info/problem/lubenica
+
 #include <iostream>
 #include <vector>
 using namespace std;
 
 #define N 100010
-const int inf = 1e9;
-
-vector <int> a[N];
-int head[N],label[N],child[N],dep[N],par[N];
-int st[4*N],heavy[N],pos[N],b[N];
-int n,q,nchain,cnt;
+vector <pair <int,int>> a[N];
+int par[N],head[N],lab[N],child[N],dep[N],pos[N],w[N],b[N];
+int n,nchain;
+pair <int,int> st[4*N];
 
 void enter(){
 
-    cin >> n >> q;
-    for (int i = 1; i < n; i++){
-        int x,y;
-        cin >> x >> y;
-        a[x].push_back(y);
-        a[y].push_back(x);
+    cin >> n;
+    for (int i = 1,x,y,w; i < n; i++){
+        cin >> x >> y >> w;
+        a[x].push_back({y,w});
+        a[y].push_back({x,w});
     }
 }
-void DFS(int u,int parent){
+void DFS(int u,int p){
 
-	par[u] = parent;
-    dep[u] = dep[parent] + 1;
-    for (int v : a[u]){
-		if (v == parent) continue;
-        DFS(v,u);
-        if (!heavy[u] || child[v] > child[heavy[u]]) heavy[u] = v;
-        child[u] += child[v]+1;
+    par[u] = p;
+    dep[u] = dep[p] + 1;
+    for (int i = 0; i < a[u].size(); i++){
+        int v = a[u][i].first;
+        if (v != p){
+            DFS(v,u);
+            w[v] = a[u][i].second;
+            if (a[u][0].first == p || child[v] > child[a[u][0].first]) swap(a[u][0],a[u][i]);
+            child[u] += child[v]+1;
+        }
     }
 }
-void hld(int u,int parent){
+void HLD(int u,int p){
 
-    if (head[nchain] == 0) head[nchain] = u;
-    label[u] = nchain;
-    pos[u] = ++cnt;
-    b[cnt] = u;
+    if (!head[nchain]) head[nchain] = u;
+    pos[u] = ++pos[0];
+    b[pos[u]] = w[u];
+    lab[u] = nchain;
+    if (a[u][0].first != p) HLD(a[u][0].first,u);
+    for (int i = 1; i < a[u].size(); i++){
+        if (a[u][i].first != p) nchain++, HLD(a[u][i].first,u);
+    }
+}
+pair <int,int> com(pair <int,int> P,pair <int,int> Q){
 
-    if (!heavy[u]) return;
-    hld(heavy[u],u);
-
-    for (int v : a[u])
-		if (v != heavy[u] && v != parent) nchain++,hld(v,u);
+    return make_pair(max(P.first,Q.first), min(P.second,Q.second));
 }
 void build(int id,int l,int r){
 
-    if (l == r){
-        st[id] = inf;
-        return;
-    }
+    st[id] = {b[l],b[l]};
+    if (l == 1) st[id] = make_pair(0,1e9);
+    if (l == r) return;
     int m = (l+r) >> 1;
     build(id*2,l,m);
     build(id*2+1,m+1,r);
-    st[id] = inf;
+    st[id] = com(st[id*2],st[id*2+1]);
 }
-void update_st(int id,int l,int r,int p){
+pair <int,int> getST(int id,int l,int r,int x,int y){
 
-    if (l > p || r < p) return;
-    if (l == r){
-		if (st[id] == inf) st[id] = l;
-			else st[id] = inf;
-        return;
-    }
-    int m = (l+r) >> 1;
-    update_st(id*2,l,m,p);
-    update_st(id*2+1,m+1,r,p);
-    st[id] = min(st[id*2],st[id*2+1]);
-}
-int get_st(int id,int l,int r,int x,int y){
-
-    if (l > y || r < x) return inf;
+    if (l > y || r < x) return make_pair(0,1e9);
     if (x <= l && r <= y) return st[id];
     int m = (l+r) >> 1;
-    return min(get_st(id*2,l,m,x,y), get_st(id*2+1,m+1,r,x,y));
+    return com(getST(id*2,l,m,x,y), getST(id*2+1,m+1,r,x,y));
 }
-int get(int x){
+pair <int,int> get(int x,int y){
 
-    int y = 1, res = inf;
-    for (;label[x] != label[y]; y = par[head[label[y]]]){
-        if (label[x] > label[y]) swap(x,y);
-        res = min(res,get_st(1,1,n,pos[head[label[y]]],pos[y]));
+    pair <int,int> ret = {0,1e9};
+    while (lab[x] != lab[y]){
+        if (lab[x] > lab[y]) swap(x,y);
+        int hy = head[lab[y]];
+        pair <int,int> tmp = getST(1,1,n,pos[hy],pos[y]);
+        ret = com(ret,tmp);
+        y = par[hy];
     }
-    if (child[x] < child[y]) swap(x,y);
-    return min(res,get_st(1,1,n,pos[x],pos[y]));
+    if (x != y){
+        if (pos[x] > pos[y]) swap(x,y);
+        pair <int,int> tmp = getST(1,1,n,pos[x]+1,pos[y]);
+        ret = com(ret,tmp);
+    }
+    return ret;
+}
+void solve(){
+
+    int q; cin >> q;
+    while (q--){
+        int x,y; cin >> x >> y;
+        pair <int,int> ans = get(x,y);
+        cout << ans.second << ' ' << ans.first << '\n';
+    }
 }
 int main(){
 
     ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
     enter();
     DFS(1,0);
-    hld(1,0);
+    HLD(1,0);
     build(1,1,n);
-    while (q--){
-        int x,y; cin >> x >> y;
-        if (x){
-            int p = get(y);
-            if (p == inf) cout << -1 << '\n';
-                else cout << b[p] << '\n';
-        }
-        else update_st(1,1,n,pos[y]);
-    }
+    solve();
     return 0;
 }
